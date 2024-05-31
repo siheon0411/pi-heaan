09ac002d5c5151bf0cce2ce6a21819c74a5e293c
Siheon Kim <siheon0411@gmail.com>
2024-05-31 13:48:38 +0900

    final edit

--- project final/Server/server.py
+++ project final/Server/server.py
@@ -312,4 +312,4 @@ def predict(data: Data):
 # Run server
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
